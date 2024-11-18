48e742074cc6c834cf8d33ea0b4d4f1d4237f32b
Wieland <29458682+wielandb@users.noreply.github.com>
2024-11-18 22:07:50 +0100

    Add translations and images

---
+++ export/html5/scripts/c3main.js
@@ -0,0 +1,4 @@
+import "./../box2d.wasm.js";
+import "./c3runtime.js";
+import "./objRefTable.js";
+import "./project/scriptsInEvents.js";
--- export/html5/scripts/project/scriptsInEvents.js
+++ export/html5/scripts/project/scriptsInEvents.js
@@ -5,72 +5,72 @@ const scriptsInEvents = {
 
 	async ["Event-Blatt1_Event11_Act1"](runtime, localVars)
 	{
-		async function fetchQuestTypesFromChangesets() {
-			const url = "https://wielandbreitfeld.de/assets/proxy/osm-proxy.php";
-		
-			const diffDataFetch = await fetch(url);
-			const diffData = await diffDataFetch.text();
-		
-		    // Extract all unique changeset numbers
-		    const changesetRegex = /changeset="(\d+)"/g;
-		    const uniqueChangesets = new Set();
-		    let match;
-		
-		    while ((match = changesetRegex.exec(diffData)) !== null) {
-		        uniqueChangesets.add(match[1]);
-		    }
-		
-		    const questTypeCounts = [];
-		
-		    for (let changeset of uniqueChangesets) {
-		        const changesetResponse = await fetch(`https://api.openstreetmap.org/api/0.6/changeset/${changeset}`);
-		        const changesetData = await changesetResponse.text();
-		
-		        const questTypeMatch = /<tag k="StreetComplete:quest_type" v="([^"]+)"/.exec(changesetData);
-		        const changesCountMatch = /changes_count="(\d+)"/.exec(changesetData);
-				const userMatch = /user="([^"]+)"/.exec(changesetData); // Regex to extract user
-				var changesetIDMatch =  /id="(\d+)"/.exec(changesetData);
-		
-		
-		        if (questTypeMatch && changesCountMatch) {
-		            var questType = questTypeMatch[1];
-		            var changesCount = parseInt(changesCountMatch[1], 10);
-					var user = userMatch[1]; // Extracted user
-					var changesetID =  changesetIDMatch[1];
-					console.log(changesetIDMatch[1]);
-		
-		
-		            questTypeCounts.push({"changesCount": changesCount, "questType": questType, "user": user, "changesetID": changesetID});
-		        }
-		    }
-		
-		    return questTypeCounts;
-		}
-		
-		function getRandomInt(max) {
-		  return Math.floor(Math.random() * max);
-		}
-		
-		const delay = ms => new Promise(res => setTimeout(res, ms));
-		
-		
-		fetchQuestTypesFromChangesets().then(questTypes => {
-		    //console.log(questTypes);
-			for (const x of questTypes)
-			{
-			let mX = getRandomInt(1850);
-			let mY = -50 - getRandomInt(300) * (x["changesCount"] * 0.5) ;
-			let questInst = runtime.objects.Quest.createInstance(0, mX, mY);
-			questInst.instVars.age = 0 - getRandomInt(19);
-			questInst.instVars.QuestName = x["questType"];
-			questInst.instVars.anzahl = x["changesCount"];
-			questInst.instVars.changeSetID =  String(x["changesetID"]);
-			questInst.instVars.user = x["user"];
-			questInst.isVisible = false;
-			} 
-		
-		});
-		
+async function fetchQuestTypesFromChangesets() {
+	const url = "https://wielandbreitfeld.de/assets/proxy/osm-proxy.php";
+
+	const diffDataFetch = await fetch(url);
+	const diffData = await diffDataFetch.text();
+
+    // Extract all unique changeset numbers
+    const changesetRegex = /changeset="(\d+)"/g;
+    const uniqueChangesets = new Set();
+    let match;
+
+    while ((match = changesetRegex.exec(diffData)) !== null) {
+        uniqueChangesets.add(match[1]);
+    }
+
+    const questTypeCounts = [];
+
+    for (let changeset of uniqueChangesets) {
+        const changesetResponse = await fetch(`https://api.openstreetmap.org/api/0.6/changeset/${changeset}`);
+        const changesetData = await changesetResponse.text();
+
+        const questTypeMatch = /<tag k="StreetComplete:quest_type" v="([^"]+)"/.exec(changesetData);
+        const changesCountMatch = /changes_count="(\d+)"/.exec(changesetData);
+		const userMatch = /user="([^"]+)"/.exec(changesetData); // Regex to extract user
+		var changesetIDMatch =  /id="(\d+)"/.exec(changesetData);
+
+
+        if (questTypeMatch && changesCountMatch) {
+            var questType = questTypeMatch[1];
+            var changesCount = parseInt(changesCountMatch[1], 10);
+			var user = userMatch[1]; // Extracted user
+			var changesetID =  changesetIDMatch[1];
+			console.log(changesetIDMatch[1]);
+
+
+            questTypeCounts.push({"changesCount": changesCount, "questType": questType, "user": user, "changesetID": changesetID});
+        }
+    }
+
+    return questTypeCounts;
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+const delay = ms => new Promise(res => setTimeout(res, ms));
+
+
+fetchQuestTypesFromChangesets().then(questTypes => {
+    //console.log(questTypes);
+	for (const x of questTypes)
+	{
+	let mX = getRandomInt(1850);
+	let mY = -50 - getRandomInt(300) * (x["changesCount"] * 0.5) ;
+	let questInst = runtime.objects.Quest.createInstance(0, mX, mY);
+	questInst.instVars.age = 0 - getRandomInt(19);
+	questInst.instVars.QuestName = x["questType"];
+	questInst.instVars.anzahl = x["changesCount"];
+	questInst.instVars.changeSetID =  String(x["changesetID"]);
+	questInst.instVars.user = x["user"];
+	questInst.isVisible = false;
+	} 
+
+});
+
 	}
 
 };
